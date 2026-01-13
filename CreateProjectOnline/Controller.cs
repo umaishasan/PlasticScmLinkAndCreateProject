e1163ff07242285ec1d5db2ef40601cb554fe5ed
UmaisHasan <umaishasan96@gmail.com>
2026-01-13 16:30:16 +0500

    Initialize Project

---
+++ CreateProjectOnline/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateProjectOnline
+{
+    public class Controller
+    {
+        public string server = "@cloud";
+        public string selectOrganization;
+        public string projectName;
+        public string projectLocation;
+
+        public Controller(string selectOrganization, string projectName,string projectLocation)
+        {
+            this.selectOrganization = selectOrganization;
+            this.projectName = projectName;
+            this.projectLocation = projectLocation;
+        }
+
+        public void CreateProjectOnline()
+        {
+            // string repoName = "MyAutoRepo";
+            string serverName = selectOrganization + this.server; // or your server name
+            Debug.WriteLine($"Starting Plastic SCM Automation...{serverName}");
+
+            RunCmd($"cm mkrep {projectName}@{serverName}");
+            RunCmd($"cm mkws {projectName}_ws {projectLocation} --repository={projectName}@{serverName}");
+            // Create default project files
+            File.WriteAllText(Path.Combine(projectLocation, "README.md"), "# Default Project");
+            RunCmd($"cm add \"{projectLocation}\"");
+            RunCmd($"cm checkin -m \"Initial commit\" \"{projectLocation}\"");
+
+            Debug.WriteLine("Automation completed successfully!");
+            /*Console.ReadKey();
+            Console.ReadKey();*/
+        }
+
+        private void RunCmd(string command)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + command;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            process.WaitForExit();
+        }
+    }
+}
---
+++ CreateProjectOnline/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Windows;
+using CreateProjectOnline;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace CreateProjectOnline
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller _controller;
+        public string selectOrganization;
+        public string projectName;
+        public string projectLocation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            string[] selectOrg = {""};
+            if (comboBox != null)
+            {
+                selectOrg = comboBox.SelectedItem.ToString().Split(':');
+                selectOrganization = selectOrg[1];
+            }
+            Debug.WriteLine("What is in dd: "+selectOrganization);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Corrected code: OpenFolderDialog does not support 'Filter'. Removed the invalid property.  
+            var dialog = new Microsoft.Win32.OpenFolderDialog
+            {
+                Title = "Select the project folder"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                string selectedPath = dialog.FolderName; // Use FolderName to get the selected folder path.  
+                Debug.WriteLine($"Selected folder: {selectedPath}");
+                // Optionally, assign to your projectLocation variable.  
+                this.projectLocation = selectedPath;
+                ProjectLocationTxt.Text = this.projectLocation;
+            }
+        }
+
+        private void ProjectNameChanged(object sender, TextChangedEventArgs e)
+        {
+            // Get the TextBox that triggered the event
+            var textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                projectName = textBox.Text;
+                Debug.WriteLine($"Project Name: {projectName}");
+            }
+        }
+
+        private void CreateProject(object sender, RoutedEventArgs e)
+        {
+            _controller = new Controller(selectOrganization, projectName, projectLocation);
+            _controller.CreateProjectOnline();
+        }
+    }
+}
